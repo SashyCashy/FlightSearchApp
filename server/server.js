20761fb00ffa663436fed5d274babbbdaa1bf6c5
Sashank Pindiproli <sashankpindiproli@Sashanks-MacBook-Pro-2.local>
2020-07-11 21:42:51 +0530

    Added concurrent package to run both node and react

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-  console.log(publicPath);
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/', (req, res) => {
+  res.render('index');
 });
 app.listen(port, () => {
   console.log('Server is up!');
