fa02cc6ea54f5adc61ac5442a60f36a2dfe9423d
Sashank Pindiproli <sashankpindiproli@Sashanks-MacBook-Pro-2.local>
2020-07-11 22:20:19 +0530

    Added engines for package.json

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 8080;
 app.use(express.static(publicPath));
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
 app.listen(port, () => {
   console.log('Server is up!');
 });
