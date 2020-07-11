3b93cd8227ab99d68b137ab8636b357dae37f4ef
Sashank Pindiproli <sashankpindiproli@Sashanks-MacBook-Pro-2.local>
2020-07-12 00:15:11 +0530

    Removed extra code

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //production mode
 //if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
   res.sendfile(path.join(__dirname + '/build/index.html'));
-});
+});*/
 //}
 
 app.get('/api/customers', async (req, res) => {
