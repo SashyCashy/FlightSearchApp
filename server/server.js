0af038c98d90a342545c8a106e3a547ba22383eb
Sashank Pindiproli <sashankpindiproli@Sashanks-MacBook-Pro-2.local>
2020-07-11 22:11:15 +0530

    Changed port number

--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 6000;
 app.use(express.static(publicPath));
-app.get('/', (req, res) => {
-  res.render('index');
-});
 app.listen(port, () => {
   console.log('Server is up!');
 });
